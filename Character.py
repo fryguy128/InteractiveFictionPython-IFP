902814e52d86f05bc376e0a11e9db6a3c8939305
fryguy128 <fryguy@fryguy128.com>
2016-12-20 22:26:08 -0600

    Further developed Character and More documentation

--- Character.py
+++ Character.py
@@ -2,7 +2,38 @@
 
 
 class Character:
-    def __init__(self, wearing, health, armedWith):
+    def __init__(self, wearing, health, weapon, shield):
         self._wearing = wearing
         self._health = health
-        self._armedWith = armedWith
+        self._weapon = weapon
+        self._shield = shield
+
+    # This method causes damage to be applied to a player by decreasing their health
+    def injured(self, damage):
+        injure = True
+        try:
+            if damage < 0:
+                print("Negative damage is not possible!")
+                injure = False
+        except ValueError:
+            print("That much damage is not possible!")
+            injure = False
+        if injure:
+            self._health -= damage
+
+    # This method will heal a character a specified amount
+    def heal(self, healed):
+        canHeal = True
+        try:
+            if healed < 0:
+                print("Negative healing is not possible!")
+                canHeal = False
+        except ValueError:
+            print("That much healing is not possible!")
+            canHeal = False
+        if canHeal:
+            self._health += healed
+
+    # Returns the amount of health a player has remaining
+    def getHP(self):
+        return self._health
